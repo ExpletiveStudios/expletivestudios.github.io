cc49f2e880a37b70b15bb3cc7d52de4fb7faddc0
Patrick Petrushonis <pwp904@gmail.com>
2016-08-04 21:21:08 -0400

    Added basic analytics tracking code.

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,6 +2,9 @@
 // Bower components
 // =require ../bower_components/jquery/dist/jquery.min.js
 
+// Google analytics
+// =require partials/analytics.js
+
 // Custom JavaScript partials
 // =require partials/footer.js
 // =require partials/header.js
