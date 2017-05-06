c95981558ea7c0854951da4e7626430127f3301a
Joe <ironictrout@gmail.com>
2017-05-06 02:14:39 -0400

    Team page tweaks

--- dev/js/partials/team.js
+++ dev/js/partials/team.js
@@ -1,16 +1,19 @@
 $("#member-1").click(function(){
+	$("#info-0").hide();
 	$("#info-1").show();
 	$("#info-2").hide();
 	$("#info-3").hide();
 });
 
 $("#member-2").click(function(){
+	$("#info-0").hide();
 	$("#info-1").hide();
 	$("#info-2").show();
 	$("#info-3").hide();
 });
 
 $("#member-3").click(function(){
+	$("#info-0").hide();
 	$("#info-1").hide();
 	$("#info-2").hide();
 	$("#info-3").show();
